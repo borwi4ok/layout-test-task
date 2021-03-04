56701896efaeaa117cc58fc3a66fb02987e6a1dd
borwi4ok <donnos98@gmail.com>
2021-03-04 23:16:38 +0300

    1920w back intro

--- src/components/introBlock/Intro.js
+++ src/components/introBlock/Intro.js
@@ -1,7 +1,27 @@
 import './Intro.css'
 
 function Intro() {
-  return <div className='intro'>adasdd</div>
+  const headerOptions = ['PRODUCTS', 'TEST DRIVE', 'FOR KIDS', 'GAMES', 'CONTACT US', 'FAQ']
+
+  function getHeaderOptions() {
+    return headerOptions.map((option) => <li key={option + Math.random()}>{option}</li>)
+  }
+  return (
+    <div className='intro-container'>
+      <div className='intro'>
+        <div className='intro-blob'></div>
+        <div className='intro-header'>
+          <div className='intro-header_logo'></div>
+          <ul>{getHeaderOptions()}</ul>
+        </div>
+      </div>
+      <div className='polygon1'></div>
+      <div className='polygon2'></div>
+      <div className='polygon3'></div>
+      <div className='polygon4'></div>
+      <div className='intro-wave'></div>
+    </div>
+  )
 }
 
 export default Intro
